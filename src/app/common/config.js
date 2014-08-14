6b79c52713b8eb5f91b76b81401da2fd8da1e668
erik <erik@unswornindustries.com>
2014-08-14 15:10:53 +0200

    Dynamic baseUrl.

--- src/app/common/config.js
+++ src/app/common/config.js
@@ -5,7 +5,7 @@ angular.module('sp.player.common.config', [])
     number: '0.6.0',
     name: 'Red Eyes Player UI'
   },
-  // TODO: Make apiBase depend on environment!
-  apiBase: 'http://api.storypalette.dev:8888/v1/',
+  environment: window.env.environment || 'local',
+  apiBase: window.env.apiBase
 })
 ;
