433b47a606b2df99dcbd1e10510617ebf1c2a0ff
erik <erik@unswornindustries.com>
2016-08-01 15:57:06 +0200

    Use heroku url for api, for now

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
 const productionConfig = {
-  apiBase: 'http://api.storypalette.net/v1/',
-  socketBase: 'http://api.storypalette.net/',
+  //apiBase: 'http://api.storypalette.net/v1/',
+  //socketBase: 'http://api.storypalette.net/',
+  apiBase: 'http://storypalette-server.herokuapp.com/v1/',
+  socketBase: 'http://storypalette-server.herokuapp.com/',
   environment: 'production',
   port: 8881,
 };
